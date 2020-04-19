b4ba54347cd0dc273d205910b16ff0fce466e1a5
Benedikt Schneider <ff142270@gmail.com>
2020-04-19 12:16:52 +0200

    Find the mistake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DVDlistComponent } from './components/dvdlist/dvdlist.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +16,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
+  public titel: string = "Hallo Benutzer !";
+  public showalert: boolean = true;
+  public isValid: boolean = true;
+  public newDvdTitle: string;
+  public filme: string[] = [
+    "Film 1", "Film2", "Film 3"
+  ];
   constructor() { }
 
   ngOnInit(): void {
+    this.filme.push("Film4")
   }
 
+  public transformDvdTitle(title): string {
+    return "DVD :" + title;
+  }
+  //public onButtonClicked(): void { }
+
+  public addNewDvd(): void {
+    //this.showAlert
+    this.filme.push(this.newDvdTitle)
+    this.newDvdTitle = "";
+  }
+  public typed(): void {
+    this.isValid = this.newDvdTitle.length >= 25 ? false : true
+  }
 }
